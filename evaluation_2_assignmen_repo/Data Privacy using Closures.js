a00cb205d8df69445197903d3e6d4cdfef933ee8
kkasana <kkasanacoder@gmail.com>
2025-05-03 22:31:56 +0530

    Data Privacy using Closures

---
+++ evaluation_2_assignmen_repo/Data Privacy using Closures.js
@@ -0,0 +1,27 @@
+
+function createCounter()
+{
+  let count = 0
+  return {
+    
+    increment()
+    {
+      count++
+      return count
+    },
+    
+    getCount()
+    {
+      return count
+    }
+     
+  }
+}
+
+const counter = createCounter();
+
+console.log(counter.increment()); // Output: 1;
+
+console.log(counter.increment()); // Output: 2;
+
+console.log(counter.getCount()); // Output: 2;
